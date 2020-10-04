72398619891b23a5d0df40d226025cf8e8c29787
Thamy Bessa <thamybessa@dcc.ufrj.br>
2020-10-04 15:00:29 -0300

    password recovery page done - needs to be integrated with backend

--- src/Components/Login/LostPassword.js
+++ src/Components/Login/LostPassword.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import useForm from '../../hooks/useForm';
+import Button from '../Form/Button';
+import Input from '../Form/Input';
 
 const LostPassword = () => {
+  const email = useForm('email');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  const estilo = {
+    marginTop: '60px',
+    marginBottom: '60px',
+    textAlign: 'center',
+  };
+
   return (
     <div>
-      <h2>Recuperar Senha</h2>
+      <h2 style={estilo}>Recuperar Senha</h2>
+      <form onSubmit={handleSubmit}>
+        <Input
+          id="email"
+          label="E-mail"
+          type="text"
+          required="required"
+          {...email}
+        />
+        <Button>ENVIAR</Button>
+      </form>
     </div>
   );
 };
